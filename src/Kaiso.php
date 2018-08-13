590320e70f24d653bfcfb6c174fd3809a415895b
Chris Van Patten <chris@vanpattenmedia.com>
2018-08-13 18:16:40 -0400

    updates based on Psalm and PHPCS

--- src/Kaiso.php
+++ src/Kaiso.php
@@ -1,155 +1,169 @@
 <?php
 
-namespace TomodomoCo;
+namespace Tomodomo;
 
 use GuzzleHttp\Psr7;
 
-/*
- * Kaiso is the meat and potatoes of this whole operation.
- */
-class Kaiso {
-	/**
-	 * Default settings
-	 *
-	 * @var array
-	 */
-	public $settings = [];
+class Kaiso
+{
+    /**
+     * Default settings
+     *
+     * @var array
+     */
+    public $settings = [];
 
-	/**
-	 * Pimple container!
-	 *
-	 * @var Pimple
-	 */
-	public $container;
+    /**
+     * Pimple container!
+     *
+     * @var \Pimple\Container
+     */
+    public $container;
 
-	/**
-	 * Might be nice to have some settings here!
-	 *
-	 * @return void
-	 */
-	public function __construct($settings = []) {
-		// Merge defaults with custom settings
-		$this->settings = array_merge($this->settings, $settings);
+    /**
+     * Might be nice to have some settings here!
+     *
+     * @param array $settings
+     *
+     * @return void
+     */
+    public function __construct($settings = [])
+    {
+        // Merge defaults with custom settings
+        $this->settings = array_merge($this->settings, $settings);
 
-		// Create our Pimple container
-		$this->container = new \Pimple\Container();
+        // Create our Pimple container
+        $this->container = new \Pimple\Container();
 
-		// Add wp_query to the container so it can be used later
-		$this->container['wp_query'] = function () {
-			global $wp_query;
+        /**
+         * Add wp_query to the container so it can be used later
+         *
+         * @psalm-suppress MissingClosureReturnType
+         */
+        $this->container['wp_query'] = function () {
+            global $wp_query;
 
-			return $wp_query;
-		};
-	}
+            return $wp_query;
+        };
 
-	/**
-	 * Fetch the template hierarchy array from brain/hierarchy
-	 *
-	 * @return array
-	 */
-	public function getTemplateHierarchy()
-	{
-		$hierarchy = new \Brain\Hierarchy\Hierarchy();
+        return;
+    }
 
-		return $hierarchy->getTemplates($this->container['wp_query']);
-	}
+    /**
+     * Fetch the template hierarchy array from brain/hierarchy
+     *
+     * @return array
+     */
+    public function getTemplateHierarchy()
+    {
+        $hierarchy = new \Brain\Hierarchy\Hierarchy();
 
-	/**
-	 * Format WP-style template names into FancyPhpController names.
-	 *
-	 * @return string
-	 */
-	public function formatControllerName($name)
-	{
-		return (string) \Stringy\Stringy::create($name)
-			->removeRight('.php')
-			->upperCamelize()
-			->append('Controller');
-	}
+        return $hierarchy->getTemplates($this->container['wp_query']);
+    }
 
-	/**
-	 * Iterate over the template hierarchy and format it
-	 * as controllers and paths
-	 *
-	 * @return array
-	 */
-	public function getControllerHierarchy()
-	{
-		$templates = $this->getTemplateHierarchy();
+    /**
+     * Format WP-style template names into FancyPhpController names.
+     *
+     * @param string $name
+     *
+     * @return string
+     */
+    public function formatControllerName($name)
+    {
+        return (string) \Stringy\Stringy::create($name)
+            ->removeRight('.php')
+            ->upperCamelize()
+            ->append('Controller');
+    }
 
-		foreach ($templates as $template) {
-			$controllers[] = $this->settings['controllerPath'] . $this->formatControllerName($template);
-		}
+    /**
+     * Iterate over the template hierarchy and format it
+     * as controllers and paths
+     *
+     * @return array
+     */
+    public function getControllerHierarchy()
+    {
+        $templates = $this->getTemplateHierarchy();
 
-		return $controllers;
-	}
+        $controllers = [];
 
-	/**
-	 * Find a working controller, or throw an exception
-	 *
-	 * @return Controller
-	 */
-	public function getController()
-	{
-		// Fetch our formatted list of controller names
-		$controllers = $this->getControllerHierarchy();
+        foreach ($templates as $template) {
+            $path = $this->settings['controllerPath'];
+            $name = $this->formatControllerName($template);
 
-		// Empty controller to fill in later
-		$controller = null;
+            $controllers[] = $path . $name;
+        }
 
-		// Loop through the possible controllers
-		foreach ($controllers as $controllerName) {
-			// Check if it exists; instantiate if so
-			if (class_exists($controllerName)) {
-				$controller = new $controllerName($this->container);
+        return $controllers;
+    }
 
-				break;
-			}
-		}
+    /**
+     * Find a working controller, or throw an exception
+     *
+     * @return object
+     */
+    public function getController()
+    {
+        // Fetch our formatted list of controller names
+        $controllers = $this->getControllerHierarchy();
 
-		// If we didn't get a controller, throw an exception
-		if ($controller === null) {
-			// @todo Use a specific Exception
-			throw new \Exception();
-		}
+        // Empty controller to fill in later
+        $controller = null;
 
-		return $controller;
-	}
+        // Loop through the possible controllers
+        foreach ($controllers as $controllerName) {
+            // Check if it exists; instantiate if so
+            if (class_exists($controllerName)) {
+                $controller = new $controllerName($this->container);
 
-	/**
-	 * The heart of the operation — run the app
-	 *
-	 * @return void
-	 */
-	public function run()
-	{
-		// @todo handle an exception here
-		$controller = $this->getController();
+                break;
+            }
+        }
 
-		// Grab the server request object
-		$request = Psr7\ServerRequest::fromGlobals();
+        // If we didn't get a controller, throw an exception
+        if ($controller === null) {
+            // @todo Use a specific Exception
+            throw new \Exception();
+        }
 
-		// @todo Massage WordPress to use a PSR7-compatible response
-		$response = null;
+        return $controller;
+    }
 
-		// Pass along query args
-		$args = $request->getQueryParams();
+    /**
+     * The heart of the operation — run the app
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // @todo handle an exception here
+        $controller = $this->getController();
 
-		// Get the request method
-		$method = strtolower($_SERVER['REQUEST_METHOD']);
+        // Grab the server request object
+        $request = Psr7\ServerRequest::fromGlobals();
 
-		// If the method `any` exists on our found controller, load that.
-		// Otherwise, we load a method matching the request method (e.g.
-		// get(), post(), etc.
-		if (method_exists($controller, 'any')) {
-			echo $controller->any($request, $response, $args);
-		} else if (method_exists($controller, $method)) {
-			echo $controller->{$method}($request, $response, $args);
-		} else {
-			// @todo Use a specific Exception
-			throw new \Exception();
-		}
+        // @todo Massage WordPress to use a PSR7-compatible response
+        $response = null;
 
-		exit;
-	}
+        // Pass along query args
+        $args = $request->getQueryParams();
+
+        // Get the request method
+        $method = strtolower($_SERVER['REQUEST_METHOD']);
+
+        // If the method `any` exists on our found controller, load that.
+        // Otherwise, we load a method matching the request method (e.g.
+        // get(), post(), etc.
+        if (method_exists($controller, 'any')) {
+            echo $controller->any($request, $response, $args);
+        } elseif (method_exists($controller, $method)) {
+            echo $controller->{$method}($request, $response, $args);
+        } else {
+            // @todo Use a specific Exception
+            throw new \Exception();
+        }
+
+        exit;
+    }
 }
--- src/Kaiso/Controllers/BaseController.php
+++ src/Kaiso/Controllers/BaseController.php
@@ -1,19 +1,25 @@
 <?php
 
-namespace TomodomoCo\Kaiso\Controllers;
+namespace Tomodomo\Kaiso\Controllers;
 
-class BaseController {
-	/**
-	 * @var Pimple\Container
-	 */
-	public $container;
+class BaseController
+{
+    /**
+     * The Pimple container for this controller
+     *
+     * @var \Pimple\Container
+     */
+    public $container;
 
-	/**
-	 * @param \Pimple\Container $container
-	 *
-	 * @return void
-	 */
-	public function __construct($container) {
-		$this->container = $container;
-	}
+    /**
+     * Instantiate the controller with a Container
+     *
+     * @param \Pimple\Container $container
+     *
+     * @return void
+     */
+    public function __construct(\Pimple\Container $container)
+    {
+        $this->container = $container;
+    }
 }
